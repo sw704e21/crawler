f259a6ed40c418f82dcbef004ccd3b7ce0ec3566
JohanKrogh <johan.krogh1@gmail.com>
2021-10-20 10:19:02 +0200

    Post subreddit submission data method

--- src/RedditAPI.py
+++ src/RedditAPI.py
@@ -3,27 +3,28 @@ import json
 import requests
 
 
+def initialize_reddit():
+    reddit = praw.Reddit(client_id='y9aowlfsW7dLZyFuyrpH-w',
+                     client_secret='3PSSrFjw7RX-nG6xfyFx_IFd74PHbQ',
+                     user_agent='Huften')
+    return reddit
+
 
 class RedditAPI:
 
-
     fields = ('title', 'url', 'selftext', 'score', 'created_utc', 'num_comments')
+
     def __init__(self):
         self.headline = set()
         self.submissions = set()
         self.list_of_items = []
         self.seen_submissions = set()
         self.fields
-
-    def initialize_reddit(self):
-        reddit = praw.Reddit(client_id='y9aowlfsW7dLZyFuyrpH-w',
-                         client_secret='3PSSrFjw7RX-nG6xfyFx_IFd74PHbQ',
-                         user_agent='Huften')
-        return reddit
+        self.api_url = ""
 
     def subreddit_stream(self, subreddit):
 
-        subreddit = self.initialize_reddit().subreddit(subreddit)
+        subreddit = initialize_reddit().subreddit(subreddit)
 
         for submission in subreddit.stream.submissions():
 
@@ -37,11 +38,12 @@ class RedditAPI:
             with open('data.json', 'w') as f:
                 json.dump(self.list_of_items, f)
 
+    def post_data(self):
+        f = open('data.json')
+        reddit = json.load(f)
+        r = requests.post(self.api_url + "data/reddit", data=reddit)
 
-    def print_submissions(self):
-        print(self.submissions)
-
-    def print_headlines(self):
-        print(self.headline)
-
-
+        try:
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(e)
