7bdcaff6b380a9f9a661373141fe31b1dc378531
social2dk <45236436+social2dk@users.noreply.github.com>
2021-10-27 15:21:18 +0200

    Created multithreading for creating crawlers using a csv file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
+import csv
+from multiprocessing import Process
+from RedditAPI import RedditAPI
+
+reddits_to_scrape = []
+
+def start_crawler(reddit_name):
+    crawler = RedditAPI()
+    crawler.subreddit_stream(reddit_name)
 
 if __name__ == '__main__':
-    print('Hello World')
+    with open('subreddits.csv', newline='') as f:
+        reader = csv.reader(f)
+        reddits_to_scrape = list(reader)
+    for reddit in reddits_to_scrape:
+        p = Process(target=start_crawler,args=(reddit),)
+        p.start()
