4f236e03d2ef5a62e5ab9b42a7abbb7945ceba1e
social2dk <45236436+social2dk@users.noreply.github.com>
2021-10-27 15:25:40 +0200

    Lint fixes, probably still some to go

--- src/main.py
+++ src/main.py
@@ -8,10 +8,11 @@ def start_crawler(reddit_name):
     crawler = RedditAPI()
     crawler.subreddit_stream(reddit_name)
 
+
 if __name__ == '__main__':
     with open('subreddits.csv', newline='') as f:
         reader = csv.reader(f)
         reddits_to_scrape = list(reader)
     for reddit in reddits_to_scrape:
-        p = Process(target=start_crawler,args=(reddit),)
+        p = Process(target=start_crawler, args=(reddit),)
         p.start()
