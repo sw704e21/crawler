39a54b1d0d8f9d9d4660442d980b07a893da6ed3
social2dk <45236436+social2dk@users.noreply.github.com>
2021-10-28 10:04:52 +0200

    More lint stuff because i cant read

--- src/add_subreddit.py
+++ src/add_subreddit.py
@@ -3,6 +3,5 @@ import sys
 
 
 with open('subreddits.csv', 'a', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=' ',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    writer = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     writer.writerow([sys.argv[1]])
