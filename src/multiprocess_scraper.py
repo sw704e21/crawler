77b41d656d670a9ef3c97d5099665fe3e976422e
social2dk <45236436+social2dk@users.noreply.github.com>
2021-10-28 10:40:55 +0200

    wrapped the multiprocessing in a class and renamed the file
    Lint me up daddy

---
+++ src/multiprocess_scraper.py
@@ -0,0 +1,18 @@
+import csv
+from multiprocessing import Process
+from RedditAPI import RedditAPI
+
+def start_crawler(reddit_name):
+    crawler = RedditAPI()
+    crawler.subreddit_stream(reddit_name)
+
+class MultiProcessScraper:
+    def start_scrapers(self):
+        if __name__ == '__main__':
+            with open('subreddits.csv', newline='') as f:
+                reader = csv.reader(f)
+                self.reddits_to_scrape = list(reader)
+            print(self.reddits_to_scrape)
+            for reddit in self.reddits_to_scrape:
+                p = Process(target=start_crawler, args=(reddit),)
+                p.start()
