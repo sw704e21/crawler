a12e140efdc82744d3cc067de82412ca188dd03f
social2dk <45236436+social2dk@users.noreply.github.com>
2021-10-28 11:38:09 +0200

    Fixed Adrians comment
    Added init to the multiprocess part and a if name == main in add_subreddit

--- src/add_subreddit.py
+++ src/add_subreddit.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-
-with open('subreddits.csv', 'a', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow([sys.argv[1]])
+if __name__ == '__main__':
+    with open('subreddits.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([sys.argv[1]])
--- src/multiprocess_scraper.py
+++ src/multiprocess_scraper.py
@@ -10,11 +10,14 @@ def start_crawler(reddit_name):
 
 class MultiProcessScraper:
 
+    def __init__(self):
+        with open('subreddits.csv', newline='') as f:
+            reader = csv.reader(f)
+            self.reddits_to_scrape = list(reader)
+
     def start_scrapers(self):
         if __name__ == '__main__':
-            with open('subreddits.csv', newline='') as f:
-                reader = csv.reader(f)
-                self.reddits_to_scrape = list(reader)
             for reddit in self.reddits_to_scrape:
+                print('Started thread')
                 p = Process(target=start_crawler, args=(reddit),)
                 p.start()
