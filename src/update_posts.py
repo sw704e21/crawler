33c59f60dfd409d80776323567969d38ed1bba07
JohanKrogh <johan.krogh1@gmail.com>
2021-11-10 13:23:06 +0100

    updateposts

--- src/update_posts.py
+++ src/update_posts.py
@@ -2,6 +2,11 @@ import praw
 import json
 import requests
 
+def initialize_reddit():
+    reddit = praw.Reddit(client_id='y9aowlfsW7dLZyFuyrpH-w',
+                         client_secret='3PSSrFjw7RX-nG6xfyFx_IFd74PHbQ',
+                         user_agent='Huften')
+    return reddit
 
 class UpdatePosts:
 
@@ -16,9 +21,23 @@ class UpdatePosts:
         self.id_list = ""
         self.payload = json.dumps(self.incoming_submissions)
 
-    def get_24h_old_posts(self, subreddit):
-        r = requests.get(self.api_url + f"coins/{subreddit}?age=1", params=self.payload)
-        print(r)
-        print(self.payload)
+    def patch_24h_old_posts(self, subreddit):
+        r = requests.patch(self.api_url + f"coins/{subreddit}?age=1", params=self.payload)
+        data = r.json()
+        print(data)
+
+
+    def update_data(self, data):
+        for submission in data:
+            to_dict = vars(submission)
+            url = submission['url']
+            submission_updated = initialize_reddit().submission(url=url)
+            submission['score'] = submission_updated.score
+            submission['num_comments'] = submission_updated.num_comments
+            sub_dict = {field: to_dict[field] for field in self.fields}
+            print(sub_dict)
+            # print(r)
+            # print(self.payload)
+
 up = UpdatePosts()
-up.get_24h_old_posts('bitcoin')
+up.get_24h_old_posts('btc')
