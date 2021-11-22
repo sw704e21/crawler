bc5f2340d5a37627c915e562b70c19a1e83faf58
JohanKrogh <johan.krogh1@gmail.com>
2021-11-22 14:28:39 +0100

    Concurrent processes of scraper and scheduler

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 from multiprocess_scraper import MultiProcessScraper
 from update_posts import UpdatePosts
+import multiprocessing
+import time
 
 
 if __name__ == '__main__':
     manager = MultiProcessScraper()
-    manager.run()
-
     updating = UpdatePosts()
-    updating.scheduler()
+    manager_process = multiprocessing.Process(target=manager.run)
+    scheduler_process = multiprocessing.Process(target=updating.scheduler)
+    manager_process.start()
+    scheduler_process.start()
+
+
+
