222d3ba1731bd084ea829081655bc7a9e7df42ff
JohanKrogh <johan.krogh1@gmail.com>
2021-11-22 14:31:11 +0100

    Non used import removed

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from multiprocess_scraper import MultiProcessScraper
 from update_posts import UpdatePosts
-import multiprocessing
-import time
+from multiprocessing import Process
 
 
 if __name__ == '__main__':
     manager = MultiProcessScraper()
     updating = UpdatePosts()
-    manager_process = multiprocessing.Process(target=manager.run)
-    scheduler_process = multiprocessing.Process(target=updating.scheduler)
+    manager_process = Process(target=manager.run)
+    scheduler_process = Process(target=updating.scheduler)
     manager_process.start()
     scheduler_process.start()
 
