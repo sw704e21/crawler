5ce175e3dc36f7c509634cfc93e8c1dc090b4979
social2dk <45236436+social2dk@users.noreply.github.com>
2021-11-22 11:40:25 +0100

    Added a comment

--- src/remove_subreddit.py
+++ src/remove_subreddit.py
@@ -6,6 +6,7 @@ HOST = '127.0.0.1'
 PORT = 64000
 
 if __name__ == '__main__':
+    #Command line argument should be subreddit which should no longer be tracked
     new_reddit = ['stop', sys.argv[1]]
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
