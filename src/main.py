6eb1c5cc1c53c414cabf9b2400c21fb523048469
AdrianPlesner <Adrian.plesner@gmail.com>
2021-11-24 12:55:41 +0100

    Add prices crawler

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from multiprocess_scraper import MultiProcessScraper
 from update_posts import UpdatePosts
 from multiprocessing import Process
+from PriceAPI import PriceAPI
 
 
 if __name__ == '__main__':
     manager = MultiProcessScraper()
     updating = UpdatePosts()
+    price = PriceAPI()
     manager_process = Process(target=manager.run)
     scheduler_process = Process(target=updating.scheduler)
+    price_process = Process(target=price.run)
     manager_process.start()
     scheduler_process.start()
+    price_process.start()
