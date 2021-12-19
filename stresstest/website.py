241bcfe6d5c9c491a4b35274d355402b79d88a58
AdrianPlesner <Adrian.plesner@gmail.com>
2021-12-19 16:56:23 +0100

    stress test

---
+++ stresstest/website.py
@@ -0,0 +1,36 @@
+from locust import HttpUser, task, between
+import random
+
+coins = [
+    "ETH",
+    "BTC",
+    "BAN",
+    "SOL",
+    "LINK",
+    "UNI",
+    "ADA",
+    "TRX",
+    "MATIC",
+    "DAI",
+    "LTC",
+    "SHIB",
+    "ALGO",
+    "DOGE",
+    "DOT",
+    "VET",
+    "ELON"
+]
+
+
+class CryptoUser(HttpUser):
+    wait_time = between(15, 30)
+
+    @task(2)
+    def all_coin(self):
+        # sort_param = random.choice(sort)
+        self.client.get('/#/', name="front page")
+
+    @task
+    def coin_info(self):
+        coin = random.choice(coins)
+        self.client.get(f'/#/crypto/{coin}', name=" specific view")
