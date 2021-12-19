c9c56a9140f9cf8b3060dd82288bd4290c559297
AdrianPlesner <Adrian.plesner@gmail.com>
2021-12-19 15:20:46 +0100

    Stress test and stream all of reddit

--- src/RedditAPI.py
+++ src/RedditAPI.py
@@ -28,47 +28,25 @@ class RedditAPI:
 
         reddit_url = "https://www.reddit.com"
         reddit = initialize_reddit()
-        query = tags[0]
-        for t in tags[1:]:
-            query += " OR " + t
-        last = []
-        waittime = 1
         while True:
-            i = 0
-            new = []
             try:
-                for submission in reddit.subreddit('all').search(query, sort='new', limit=100):
-                    try:
-                        if submission.id not in last:
-
-                            # Adding the specified submission fields to the json object
-                            redditor = submission.author
-                            sub_dict = {'karma': redditor.link_karma + redditor.comment_karma,
-                                        'created_utc': submission.created_utc * 1000,
-                                        'permalink': reddit_url + submission.permalink,
-                                        'uuid': submission.id,
-                                        'source': 'reddit',
-                                        'title': submission.title,
-                                        'selftext': submission.selftext,
-                                        'score': submission.score,
-                                        'num_comments': submission.num_comments}
-                            # sub_dict['source'] = subreddit.display_name
-                            # posting submission data through the API
-                            self.post_data(sub_dict)
-                            i += 1
-                        new.append(submission.id)
-                    except Exception as e:
-                        logger.error(e.args)
-                last = new
-                logger.info(f"Sent {i} new posts")
-                if i == 0 and waittime < 128:
-                    waittime *= 2
-                elif i != 0:
-                    waittime = 1
-                time.sleep(waittime)
+                for submission in reddit.subreddit("all").stream.submissions():
+                    # Adding the specified submission fields to the json object
+                    redditor = submission.author
+                    sub_dict = {'karma': redditor.link_karma + redditor.comment_karma,
+                                'created_utc': submission.created_utc * 1000,
+                                'permalink': reddit_url + submission.permalink,
+                                'uuid': submission.id,
+                                'source': 'reddit',
+                                'title': submission.title,
+                                'selftext': submission.selftext,
+                                'score': submission.score,
+                                'num_comments': submission.num_comments}
+                    # sub_dict['source'] = subreddit.display_name
+                    # posting submission data through the API
+                    self.post_data(sub_dict)
             except Exception as e:
                 logger.error(e)
-                time.sleep(60)
 
     def post_data(self, data):
         logger.info(f"Post sub {data['permalink']}")
---
+++ stresstest/api.py
@@ -0,0 +1,51 @@
+from locust import HttpUser, task, between
+import random
+sort = [
+    "mentions",
+    "-mentions",
+    "-mostInteractions",
+    "mostInteractions",
+    "relSentiment",
+    "-relSentiment",
+    "relMentions",
+    "-relMentions",
+    "displayName",
+    "-displayName",
+    "price",
+    "-price",
+    "final_score",
+    "-final_score"
+]
+
+coins = [
+    "ETH",
+    "BTC",
+    "BAN",
+    "SOL",
+    "LINK",
+    "UNI",
+    "ADA",
+    "TRX",
+    "MATIC",
+    "DAI",
+    "LTC",
+    "SHIB",
+    "ALGO",
+    "DOGE",
+    "DOT",
+    "VET",
+    "ELON"
+]
+
+
+class CryptoUser(HttpUser):
+    wait_time = between(15, 30)
+    @task(2)
+    def all_coin(self):
+        sort_param = random.choice(sort)
+        self.client.get(f'/coins/all/?sortParam={sort_param}', name="/coins/all")
+
+    @task
+    def coin_info(self):
+        coin = random.choice(coins)
+        self.client.get(f'/coins/{coin}/info', name="/coins/info")
